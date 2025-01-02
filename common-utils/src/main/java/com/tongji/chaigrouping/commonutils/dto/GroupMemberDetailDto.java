67a674b14fc22264c63da51625be95b1b86287aa
CXY <2867747371@qq.com>
2025-01-02 22:04:21 +0800

    code cleaning

--- common-utils/src/main/java/com/tongji/chaigrouping/commonutils/dto/GroupMemberDetailDto.java
+++ common-utils/src/main/java/com/tongji/chaigrouping/commonutils/dto/GroupMemberDetailDto.java
@@ -11,11 +11,4 @@ public class GroupMemberDetailDto {
     String school;
     String grade;
     String skillDescription;
-    /*  "user_id": 1,
-  "username": "Alice",
-  "role": "leader"/"member",
-  "join_date": "2024-12-01",
-  "school": "Tongji Univ.",
-  "grade": "Freshman",
-  "skillDescription": "Singing, dancing, rapping and basketball",*/
 }
