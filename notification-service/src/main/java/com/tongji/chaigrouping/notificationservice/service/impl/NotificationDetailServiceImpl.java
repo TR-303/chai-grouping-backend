04f808cec423e6adf481c540b2ac2eca03ee220f
CXY <2867747371@qq.com>
2025-01-04 03:55:09 +0800

    正常已读

--- notification-service/src/main/java/com/tongji/chaigrouping/notificationservice/service/impl/NotificationDetailServiceImpl.java
+++ notification-service/src/main/java/com/tongji/chaigrouping/notificationservice/service/impl/NotificationDetailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.tongji.chaigrouping.notificationservice.service.impl;
 
 import com.tongji.chaigrouping.commonutils.dto.NotificationDetailDto;
+import com.tongji.chaigrouping.commonutils.entity.Notification;
 import com.tongji.chaigrouping.commonutils.mapper.NotificationMapper;
 import com.tongji.chaigrouping.notificationservice.service.NotificationDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,9 @@ public class NotificationDetailServiceImpl implements NotificationDetailService 
 
     @Override
     public NotificationDetailDto readNotification(Integer notificationId) {
+        Notification notification = notificationMapper.selectById(notificationId);
+        notification.setHasRead(1);
+        notificationMapper.updateById(notification);
         return notificationMapper.getNotificationDetailById(notificationId);
     }
 
