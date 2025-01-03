4bece9668486932eef72d4ce95506220b753caa0
CXY <2867747371@qq.com>
2025-01-04 06:59:26 +0800

    创建任务时发送通知

--- task-service/src/main/java/com/tongji/chaigrouping/taskservice/TaskServiceApplication.java
+++ task-service/src/main/java/com/tongji/chaigrouping/taskservice/TaskServiceApplication.java
@@ -3,8 +3,10 @@ package com.tongji.chaigrouping.taskservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableFeignClients("com.tongji.chaigrouping.taskservice.client")
 @SpringBootApplication
 @ComponentScan("com.tongji.chaigrouping")
 @MapperScan("com.tongji.chaigrouping.commonutils.mapper")
--- task-service/src/main/java/com/tongji/chaigrouping/taskservice/service/impl/TaskCreationServiceImpl.java
+++ task-service/src/main/java/com/tongji/chaigrouping/taskservice/service/impl/TaskCreationServiceImpl.java
@@ -1,12 +1,15 @@
 package com.tongji.chaigrouping.taskservice.service.impl;
 
+import com.tongji.chaigrouping.commonutils.dto.CreateNotificationDto;
 import com.tongji.chaigrouping.commonutils.dto.TaskCreationDto;
 import com.tongji.chaigrouping.commonutils.dto.TaskDescriptionDto;
 import com.tongji.chaigrouping.commonutils.exception.AiServiceNotAvailableException;
 import com.tongji.chaigrouping.commonutils.entity.Task;
 import com.tongji.chaigrouping.aiservice.*;
+import com.tongji.chaigrouping.commonutils.mapper.GroupMapper;
 import com.tongji.chaigrouping.commonutils.mapper.MembershipMapper;
 import com.tongji.chaigrouping.commonutils.mapper.TaskMapper;
+import com.tongji.chaigrouping.taskservice.client.NotificationServiceClient;
 import com.tongji.chaigrouping.taskservice.service.TaskCreationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,18 +24,32 @@ public class TaskCreationServiceImpl implements TaskCreationService {
     private MembershipMapper membershipMapper;
     @Autowired
     private MoonshotAiUtils moonshotAiUtils;
+    @Autowired
+    private NotificationServiceClient notificationServiceClient;
+    @Autowired
+    private GroupMapper groupMapper;
 
     @Override
     public Integer createTask(Integer groupId, TaskCreationDto taskCreationDto) {
         Task task = new Task();
         task.initTask(groupId, taskCreationDto);
         taskMapper.insert(task);
+        Integer assigneeId = taskCreationDto.getAssigneeId();
+        String groupName = groupMapper.selectById(groupId).getName();
+        if (assigneeId != null) {
+            CreateNotificationDto createNotificationDto = new CreateNotificationDto(
+                    "任务分配",
+                    "您被分配了一个新任务：" + task.getDescription()+ "，请进入小组 "+groupName+" 查看详情。",
+                    null
+            );
+            notificationServiceClient.sendNotification(assigneeId, createNotificationDto);
+        }
         return task.getTaskId();
     }
 
     @Override
     public TaskMatchResponse recommendAssignee(Integer groupId, TaskDescriptionDto taskDescriptionDto) throws AiServiceNotAvailableException {
-        List<TaskMatchRequest.CandidateItem> candidates =  membershipMapper.queryTaskCandidates(groupId);
+        List<TaskMatchRequest.CandidateItem> candidates = membershipMapper.queryTaskCandidates(groupId);
         candidates.subList(0, Math.min(candidates.size(), 10)); // 不要给LLM过多信息
 
         TaskMatchRequest taskMatchRequest = new TaskMatchRequest(
@@ -43,5 +60,4 @@ public class TaskCreationServiceImpl implements TaskCreationService {
         // TODO 如何判断AI服务不可用？
         //        throw new AiServiceNotAvailableException("AI服务不可用");
     }
-
 }
