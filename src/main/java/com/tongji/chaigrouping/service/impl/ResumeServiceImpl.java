8cb4315d1b56deec6559e98a12f51f76d0c81585
cxy <2867747371@qq.com>
2025-06-21 05:33:57 +0800

    检查更新个人简介接口的参数

--- src/main/java/com/tongji/chaigrouping/service/impl/ResumeServiceImpl.java
+++ src/main/java/com/tongji/chaigrouping/service/impl/ResumeServiceImpl.java
@@ -25,8 +25,18 @@ public class ResumeServiceImpl implements ResumeService {
     @Override
     public void updateResume(Integer userId, ResumeDto resumeDto) {
         User user = userMapper.selectById(userId);
-        if(user == null) {
+        if (user == null) {
             throw new InvalidUserException("User not found");
+        }
+        if (resumeDto == null ||
+                resumeDto.getSchool() == null || resumeDto.getSchool().isEmpty() ||
+                resumeDto.getGrade() == null || resumeDto.getGrade().isEmpty() ||
+                resumeDto.getSkillDescription() == null || resumeDto.getSkillDescription().isEmpty() ||
+                !(resumeDto.getGrade().equals("大一") ||
+                        resumeDto.getGrade().equals("大二") ||
+                        resumeDto.getGrade().equals("大三") ||
+                        resumeDto.getGrade().equals("大四"))) {
+                throw new InvalidUserException("参数不合法");
         }
         user.setResume(resumeDto);
         userMapper.updateById(user);
